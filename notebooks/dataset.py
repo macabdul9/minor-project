1cbbbe537e29a026f2e75a62e45aff13f943c0fe
macabdul9 <abdulwaheed1513@gmail.com>
2020-10-02 23:49:08 +0530

    trained setup

---
+++ notebooks/dataset.py
@@ -0,0 +1,56 @@
+from torch.utils as data
+import pandas as pd
+
+
+class AbstractDataset(data.Dataset):
+    def __init__(self, file_path, sample_size, tokenizer, type_path, num_samples, input_length, output_length, print_text=False):   
+              
+        self.dataset =  pd.read_csv(path, skiprows=lambda i: i>0 and random.random() > sample_size)
+        
+        self.input_length = input_length
+        self.tokenizer = tokenizer
+        self.output_length = output_length
+        self.print_text = print_text
+  
+    def __len__(self):
+        return self.dataset.shape[0]
+    
+    def clean_text(self, text):
+
+        text = text.replace('\n','')
+        text = text.replace('``', '')
+        text = text.replace('"', '')
+        
+        return text
+    
+    
+    def convert_to_features(self, example_batch):
+        # Tokenize contexts and questions (as pairs of inputs)
+        
+        if self.print_text:
+            print("Input Text: ", self.clean_text(example_batch['text']))
+#         input_ = self.clean_text(example_batch['text']) + " </s>"
+#         target_ = self.clean_text(example_batch['headline']) + " </s>"
+        
+        input_ = self.clean_text(example_batch['text'])
+        target_ = self.clean_text(example_batch['headline'])
+        
+        source = self.tokenizer.batch_encode_plus([input_], max_length=self.input_length, 
+                                                     padding='max_length', truncation=True, return_tensors="pt")
+        
+        targets = self.tokenizer.batch_encode_plus([target_], max_length=self.output_length, 
+                                                     padding='max_length', truncation=True, return_tensors="pt")
+    
+       
+        return source, targets
+  
+    def __getitem__(self, index):
+        source, targets = self.convert_to_features(self.dataset[index])
+        
+        source_ids = source["input_ids"].squeeze()
+        target_ids = targets["input_ids"].squeeze()
+
+        src_mask    = source["attention_mask"].squeeze()
+        target_mask = targets["attention_mask"].squeeze()
+
+        return {"source_ids": source_ids, "source_mask": src_mask, "target_ids": target_ids, "target_mask": target_mask}
